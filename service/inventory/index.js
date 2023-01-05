cdeba95255d1486195470b8965820c59018966d6
kgk_cc <pankaj.soni@kgkmail.com>
2023-01-05 09:12:27 +0530

    Initial Commit

---
+++ service/inventory/index.js
@@ -0,0 +1,51 @@
+const { buildSubgraphSchema } = require('@apollo/subgraph');
+const { ApolloServer } = require('@apollo/server');
+const { startStandaloneServer } = require('@apollo/server/standalone');
+const {gql} = require('graphql-tag');
+const typeDefs = gql`
+    extend type Product @key(fields: "upc"){
+        upc: String! @external
+        weight: Int @external
+        price: Int @external
+        inStock: Boolean
+        shippingEstimate: Int @requires(fields: "price weight")
+    }
+`;
+
+const inventory = [
+    { upc: "1", inStock: true },
+    { upc: "2", inStock: false },
+    { upc: "3", inStock: true }
+];
+
+const resolvers = {
+    Product: {
+        __resolverReference(object) {
+            return {
+                ...object,
+                ...inventory.find(inv => inv.upc === object.upc)
+            };
+        },
+        shippingEstimate(object) {
+            if (object.price > 1000) return 0;
+            return object.weight * 0.5;
+        }
+    }
+};
+
+const server = new ApolloServer({
+    schema: buildSubgraphSchema({
+        typeDefs,
+        resolvers
+    })
+});
+
+(async () => {
+    const {url} = await startStandaloneServer(server, {
+        listen: {
+            port: 5002
+        }
+    });
+
+    console.log(`Inventory service is ready at ${url}`);
+})();
